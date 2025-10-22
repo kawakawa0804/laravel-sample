7dee19079f195676314ae42582f67f5ef7532ec5
川西栄也 <fugutapc@kawanishihisashinarinoMacBook-Air.local>
2025-10-22 14:14:25 +0900

    feat: add Post model and PostController (resource) with routes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+use App\Http\Controllers\PostController;
+Route::resource('posts', PostController::class);  // 一括ルーティング
